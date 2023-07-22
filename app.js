f2a79dbda0ee6f70828202257a2188ab26c6c6ec
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-07-22 21:46:59 +0530

    react 3 lacture

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom/client"
+
+const heading=
+(
+<h1>
+    
+    hellow world
+</h1>
+);
+const Title=()=>{
+    retrun (
+        <>
+        
+        {heading}
+        <h1 id="title">
+            React Aapp
+        </h1>
+        </>
+    )
+}
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(Title)
+
+
